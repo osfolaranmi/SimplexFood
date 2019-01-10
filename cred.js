cb4c15a33df2a1643880c96e4f8a78c790128f8d
Folaranmi Oluwatosin Simeon <osfolaranmi@gmail.com>
2019-01-10 13:54:14 +0100

    Update cred.js

--- cred.js
+++ cred.js
@@ -1,14 +1,14 @@
 module.exports = {
     AT:{
-        apiKey: '39648c31addd2dfb868ec99e83421a0829afdeb9d01bd452d79142a60f44d101',
-        username: 'sandbox',
+        apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        username: 'xxxxxxxxxxx',
         format: 'json'
     },
     pusher:{
-       appId: '348780',
-       key: '80493e20ea0f59dafb82',
-       secret: "9c496e77bc59382eb295",
-       cluster: "mt1",
+       appId: 'xxxxxxxxxxxxx',
+       key: 'xxxxxxxxxxxxxx',
+       secret: "xxxxxxxxxxxxx",
+       cluster: "en",
        encrypted: true
     }
    }
